3a95738c14e40e187ae128b5972e0f02375ca4e8
root <root@localhost.localdomain>
2018-09-11 22:11:42 +0800

    Initial Comment Of My Own

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Speech recocnition</title>
+    <script src="/libraries/p5.js" type="text/javascript"></script>
+    <script src="/libraries/p5.dom.js" type="text/javascript"></script>
+    <script src="/libraries/p5.sound.js" type="text/javascript"></script>
+    <script src="/libraries/p5.speech.js" type="text/javascript"></script>
+    <script src="sketch.js" type="text/javascript"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
